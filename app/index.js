5f9d3224e3cb9fe26fcb189c54db8fcb134a4865
vijayg-mediwave <vijay_g@mindwaveventures.com>
2023-01-12 15:48:36 +0530

    port

--- app/index.js
+++ app/index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.listen(4567, (err) => {
+  if (err) {
+    console.log(`app is not running${err}`);
+  } else {
+    console.log(`app running successfully`);
+  }
+});
